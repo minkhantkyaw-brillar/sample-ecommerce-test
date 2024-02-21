427437b2099293fa2f083ab50ef1d008f699674b
MinKhantKyaw <minkhant.kyaw@brillar.io>
2024-02-21 16:27:13 +0800

    Changed backend URL to hosted URL

--- client/src/request-methods.js
+++ client/src/request-methods.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
-const BASE_URL = "http://192.168.0.54:1234/api";
+const BASE_URL = import.meta.env.VITE_BASE_URL;
+console.log(import.meta.env.VITE_BASE_URL);
 const TOKEN =
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYzMGEzOGUwYjczZTg5NTczZDAxNmZmNSIsImlzQWRtaW4iOnRydWUsImlhdCI6MTY2MzA3NTI3OSwiZXhwIjoxNjYzMTYxNjc5fQ.sLeBTfgAymobkEF1rcOzexB8-ixEXKWat-7lRFfaKVQ";
 
